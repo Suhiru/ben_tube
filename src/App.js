f3acc3e8357749d0308abab88b498488a2e55917
Suhiru DESILVA <suhiruben@gmail.com>
2024-08-11 18:42:22 +0200

    before sidebar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from './components';
 
-const App = () => {
+const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
@@ -13,7 +20,6 @@ const App = () => {
         <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
       </Routes>
     </Box>
-  </BrowserRouter>;
-};
-
+  </BrowserRouter>
+);
 export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const Sidebar = () => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
